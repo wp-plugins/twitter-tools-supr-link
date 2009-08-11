dffd4f0175d5b5707a45f82a17661370cad27f35
jonrogersuk <jonrogersuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-11 22:36:57 +0000

    tagging version 1.3 with support for links on your own domain
    git-svn-id: https://plugins.svn.wordpress.org/twitter-tools-supr-link/tags/0.1.3@144916 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,55 @@
+=== Twitter Tools: Su.pr Links ===
+Contributors: Jon Rogers
+Donate link: http://su.pr/1PPfE2
+Tags: twitter
+Requires at least: 2.7
+Tested up to: 2.8
+Stable tag: 0.1.3
+
+Makes the links that Twitter Tools posts to Twitter be API-created su.pr links so you can track the number of clicks and such via your su.pr account.
+
+== Description ==
+
+[Twitter Tools](http://wordpress.org/extend/plugins/twitter-tools/) is an excellent plugin for posting notifications of new blog posts to [Twitter](http://twitter.com/). However Twitter Tools just sends the URL to the new post normally which is then shortened by Twitter itself to a [bit.ly](http://bit.ly/) short link. This is done anonymously.
+
+This plugin will replace the normal URLs sent by Twitter Tools to Twitter with su.pr URLs tied to your Stumble Upon account. You can then easily track the number of clicks from your Stumble Upon profile.
+
+This plugin is based on [twitter-tools-bitly-links](http://wordpress.org/extend/plugins/twitter-tools-bitly-links/) by Viper007Bond
+
+**Requirements**
+
+* [Twitter Tools](http://wordpress.org/extend/plugins/twitter-tools/) be installed and activated
+* PHP 5.2.0 or newer (PHP4 is dead anyway)
+* WordPress 2.7 or newer
+
+== Installation ==
+
+###Manual Installation###
+
+Extract all files from the ZIP file, **making sure to keep the file/folder structure intact**, and then upload it to `/wp-content/plugins/`.
+
+###Automated Installation###
+
+Visit Plugins -> Add New in your admin area and search for this plugin. Click "Install".
+
+**See Also:** ["Installing Plugins" article on the WP Codex](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins)
+
+###Plugin Usage###
+
+Visit Settings -> Twitter Tools: bit.ly and fill in your login and API key.
+
+== Frequently Asked Questions ==
+
+= It's not working! =
+
+Did you make sure to fill in your su.pr login and API key on the plugin's settings page?
+
+== Screenshots ==
+
+1. The settings page is where you need to add in your own su.pr login and API key - get one from su.pr, don't ask me!
+
+== ChangeLog ==
+
+= 0.1.0 =
+
+* Initial release!
---
+++ trunk/twitter-tools-supr-links.php
@@ -0,0 +1,177 @@
+<?php /*
+
+**************************************************************************
+
+Plugin Name:  Twitter Tools: su.pr Links
+Plugin URI:   http://www.jonrogers.co.uk/su-pr-for-wordpress-twitter-tools/
+Version:      0.1.3
+Description:  Makes the links that <a href="http://wordpress.org/extend/plugins/twitter-tools/">Twitter Tools</a> posts to Twitter be API-created <a href="http://su.pr/">su.pr</a> links so you can track the number of clicks and such via your su.pr account. Requires PHP 5.2.0+. 
+Author:       Jon Rogers
+Author URI:   http://www.jonrogers.co.uk/
+
+*************************************************************************
+Apologies to Viper007Bond whose code I blatantly ripped!*/
+
+class TwitterToolsSuprLinks {
+
+	// Initalize the plugin by registering the hooks
+	function __construct() {
+		// This plugin requires PHP 5.2.0+ and WordPress 2.7+
+		if ( !function_exists('json_decode') || !function_exists('wp_remote_retrieve_body') )
+			return;
+
+		// Load localization domain
+		load_plugin_textdomain( 'twitter-tools-supr-links', false, '/twitter-tools-supr-links/localization' );
+
+		// Register options
+		add_option( 'viper_ttbl_login' );
+		add_option( 'viper_ttbl_apikey' );
+
+		// Register hooks
+		add_action( 'admin_menu',               array(&$this, 'register_settings_page') );
+		add_action( 'wp_ajax_viper_ttbl_check', array(&$this, 'ajax_authentication_check') );
+		add_filter( 'whitelist_options',        array(&$this, 'whitelist_options') );
+		add_filter( 'tweet_blog_post_url',      array(&$this, 'modify_url') );
+
+		// Make sure the user has filled in their login and API key
+		$login  = trim( get_option('viper_ttbl_login') );
+		$apikey = trim( get_option('viper_ttbl_apikey') );
+		if ( ( !$login || !$apikey ) && current_user_can('manage_options') )
+			add_action( 'admin_notices',        array(&$this, 'settings_warn') );
+	}
+
+
+	// Register the settings page
+	function register_settings_page() {
+		add_options_page( __('Twitter Tools: su.pr Links', 'twitter-tools-supr-links'), __('Twitter Tools: su.pr', 'twitter-tools-supr-links'), 'manage_options', 'twitter-tools-supr-links', array(&$this, 'settings_page') );
+	}
+
+
+	// Whitelist the options to allow saving via options.php
+	function whitelist_options( $whitelist_options ) {
+		$whitelist_options['twitter-tools-supr-links'] = array( 'viper_ttbl_login', 'viper_ttbl_apikey' );
+
+		return $whitelist_options;
+	}
+
+
+	// Display a notice telling the user to fill in their su.pr details
+	function settings_warn() {
+		echo '<div class="error"><p>' . sprintf( __( '<strong>Twitter Tools: su.pr Links:</strong> You must fill in your su.pr details on the <a href="%s">settings page</a> in order for this plugin to function.', 'vipers-video-quicktags' ), admin_url('options-general.php?page=twitter-tools-supr-links') ) . "</p></div>\n";
+	}
+
+
+	// Modify the URL being sent to Twitter by Twitter Tools
+	function modify_url( $url ) {
+
+		// Make sure the user has filled in their login and API key
+		$login  = urlencode( strtolower( trim( get_option('viper_ttbl_login') ) ) );
+		$apikey = urlencode( trim( get_option('viper_ttbl_apikey') ) );
+		if ( empty($login) || empty($apikey) )
+			return $url;
+
+		// Tell su.pr to shorten the URL for us
+		$response = wp_remote_retrieve_body( wp_remote_get( "http://su.pr/api/shorten?login={$login}&apiKey={$apikey}&longUrl=" . urlencode( $url ) ) );
+
+		if ( empty($response) )
+			return $url;
+
+		// Decode the response from su.pr
+		if ( !$response = json_decode( $response, true ) )
+			return $url;
+
+		if ( !isset($response['errorCode']) || 0 != $response['errorCode'] || empty($response['results']) || empty($response['results'][$url]) || empty($response['results'][$url]['shortUrl']) )
+			return $url;
+
+		if (strlen($response['results'][$url]['hash']) > 6)
+			{
+
+			return "http://".$response['results'][$url]['hash'];
+			}
+		else
+			return $response['results'][$url]['shortUrl'];
+		
+	}
+
+
+	// Settings page
+	function settings_page() { ?>
+
+<script type="text/javascript">
+// <![CDATA[
+	function viper_ttbl_ajax() {
+		jQuery("#viper_ttbl_status").load("<?php echo admin_url('admin-ajax.php'); ?>?nocache=" + Math.random(), { action: "viper_ttbl_check", login: jQuery("#viper_ttbl_login").val(), apikey: jQuery("#viper_ttbl_apikey").val() });
+	}
+	jQuery(document).ready(function(){ viper_ttbl_ajax() });
+	jQuery("body").change(function(){ viper_ttbl_ajax() }); // I couldn't get anything but "body" to work for some reason
+// ]]>
+</script>
+
+<div class="wrap">
+<?php screen_icon(); ?>
+	<h2><?php _e( 'Twitter Tools: su.pr Links Settings', 'twitter-tools-supr-links' ); ?></h2>
+
+	<form id="viper_ttbl_form" method="post" action="options.php">
+<?php settings_fields('twitter-tools-supr-links'); ?>
+
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row"><label for="viper_ttbl_login"><?php _e( 'su.pr Login', 'twitter-tools-supr-links' ); ?></label></th>
+			<td><input type="text" name="viper_ttbl_login" id="viper_ttbl_login" value="<?php form_option('viper_ttbl_login'); ?>" class="regular-text" /></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row"><label for="viper_ttbl_apikey"><?php _e( 'su.pr API Key', 'twitter-tools-supr-links' ); ?></label></th>
+			<td>
+				<input type="text" name="viper_ttbl_apikey" id="viper_ttbl_apikey" value="<?php form_option('viper_ttbl_apikey'); ?>" class="regular-text" />
+				<span class="description"><?php printf( __( 'This can be found on your <a href="%s">account page</a>.', 'twitter-tools-supr-links' ), 'http://su.pr/settings/' ); ?></span>
+			</td>
+		</tr>
+		<tr valign="top" class="hide-if-no-js">
+			<th scope="row"><?php _e( 'API Status', 'twitter-tools-supr-links' ); ?></th>
+			<td style="font-size:1em"><span id="viper_ttbl_status"><em>Checking...</em></span></td>
+		</tr>
+	</table>
+
+	<p class="submit">
+		<input type="submit" name="twitter-tools-supr-links-submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+	</p>
+
+	</form>
+</div>
+
+<?php
+	}
+
+
+	// Check the authentication details via AJAX
+	function ajax_authentication_check() {
+		// Make sure the user has filled in their login and API key
+		$login = $apikey = false;
+		if ( !empty($_POST['login']) )
+			$login  = urlencode( strtolower( trim( $_POST['login'] ) ) );
+		if ( !empty($_POST['apikey']) )
+			$apikey = urlencode( trim( $_POST['apikey'] ) );
+		if ( empty($login) || empty($apikey) )
+			exit();
+
+		// Ask su.pr for details about a random shortened URL in order to test the authentication details
+		$response = wp_remote_retrieve_body( wp_remote_get( "http://su.pr/api/expand?shortUrl=http://su.pr/2l5JSm&login={$login}&apiKey={$apikey}" ) );
+
+		if ( empty($response) )
+			exit( '<strong style="color:red">' . __('Failed to test credentials. Hmm.', 'twitter-tools-supr-links') . '</strong>' );
+
+		// Decode the response from su.pr
+		if ( !$response = json_decode( $response, true ) )
+			exit( '<strong style="color:red">' . __('Failed to parse su.pr API response. Hmm.', 'twitter-tools-supr-links') . '</strong>' );
+
+		if ( !isset($response['errorCode']) || 0 != $response['errorCode'] )
+			exit( '<strong style="color:red">' . __('Your credentials are invalid. Please double-check them.', 'twitter-tools-supr-links') . '</strong>' );
+
+		exit( '<strong style="color:green">' . __('Your credentials are valid.', 'twitter-tools-supr-links') . '</strong>' );
+	}
+}
+
+// Start this plugin once all other plugins are fully loaded
+add_action( 'init', 'TwitterToolsSuprLinks' ); function TwitterToolsSuprLinks() { global $TwitterToolsSuprLinks; $TwitterToolsSuprLinks = new TwitterToolsSuprLinks(); }
+
+?>
